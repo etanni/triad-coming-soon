aebc569755d15f16a5ac63d16c16f88fb0324bdd
Osamah <osamah@aldoaiss.de>
2019-05-09 23:49:14 +0200

    Lambda function

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,8 +28,7 @@ const IndexPage = () => {
   const handleSubmit = async event => {
     event.preventDefault();
     const uri = 'https://us20.api.mailchimp.com/3.0/lists/484ef15c47/members';
-    const API_KEY = 'd4b7e4bd90cc6784fa41da6123fe3b2c-us20';
-    console.log(`Basic ${Buffer.from(`apikey:${API_KEY}`).toString('base64')}`);
+    const API_KEY = process.env.MAILCHIMP_API_KEY;
     const response = await axios.post(
       uri,
       {
@@ -38,11 +37,11 @@ const IndexPage = () => {
       },
       {
         headers: {
-          Accept: 'application/json',
-          Authorization: `Basic ${Buffer.from(`apikey:${API_KEY}`).toString(
-            'base64'
-          )}`,
-          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        auth: {
+          username: 'anystring',
+          password: API_KEY,
         },
       }
     );
