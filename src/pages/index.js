4045339c8ca06f27d0cd39d25feb7a9361ec010e
Osamah <osamah@aldoaiss.de>
2019-05-11 14:50:59 +0200

    Form improvments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { graphql } from 'gatsby';
 import { FaTwitter, FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 import {
