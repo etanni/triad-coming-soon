ccb9de3ffb2f9419d9ffbb75b3cf806d14b8c23e
Osamah <osamah@aldoaiss.de>
2019-05-11 14:43:46 +0200

    Image Improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Img from 'gatsby-image';
 import { FaTwitter, FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 import {
